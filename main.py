25cf51980408858c141010f51eb18983e0a035e7
Ehsan Bakefayat <75360035+bakefayat@users.noreply.github.com>
2022-11-26 15:13:23 +0330

    Create main.py
    removing empty folders.

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+path = r"E:\\some_random_path"
+
+def remove_empty_folders(path_abs):
+    walk = list(os.walk(path_abs))
+    for path, _, _ in walk[::-1]:
+        if len(os.listdir(path)) == 0:
+            os.rmdir(path)
+
+if __name__ == '__main__':
+    remove_empty_folders(path)
